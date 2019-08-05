3f94800d77d3899def3c296d057e605d8efb5b88
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2019-08-05 14:45:44 +0200

    Replace Google Analytics with basic Matomo tracking
    Additionally, these days downloads can be counted via [1] so it should
    not be a big loss.
    
    Fixes #162.
    
    [1] https://flathub.org/api/v1/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
-import { GoogleAnalyticsEventsService } from './google-analytics-events.service';
-
-declare let ga: Function;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,20 +11,11 @@ export class AppComponent {
 
   constructor(
     public router: Router,
-    private googleAnalyticsEventsService: GoogleAnalyticsEventsService
   ) {
-
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.googleAnalyticsEventsService.emitPageView(event.urlAfterRedirects);
-      }
-    });
-  }
 
   onSearch(searchTerm: string) {
     if (typeof searchTerm === 'string' && searchTerm && searchTerm.trim() && searchTerm.trim().length > 2) {
       // Track search event
-      this.googleAnalyticsEventsService.emitEvent('Search', 'SearchFromToolbar', searchTerm);
       this.router.navigate(['apps/search', searchTerm]);
     }
   }
